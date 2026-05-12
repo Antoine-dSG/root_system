ea1791573fb96aeafecba1ee722b8a47127dd9b6
Antoine de Saint Germain <adsg96@hku.hk>
2026-05-12 16:28:57 +0800

    first commit

---
+++ RootSystem/Basic.lean
@@ -0,0 +1,12 @@
+import Mathlib.Topology.Basic
+import Mathlib.Tactics
+
+/-
+Copyright (c) 2026 Antoine de Saint Germain. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Antoine de Saint Germain
+-/
+
+/-- Basic definitions used in the root system development. -/
+theorem identity (a b : ℤ) : (a+b)^2 = a^2 + 2*a*b + b^2 := by
+  ring
