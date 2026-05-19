13ed160c7aa6a11ac85803370c1266b8ebe5359f
Antoine de Saint Germain <adsg96@hku.hk>
2026-05-19 09:58:32 +0800

    An is reduced, finite, crystallographic

--- RootSystem/An/Properties.lean
+++ RootSystem/An/Properties.lean
@@ -5,14 +5,7 @@ import Mathlib.LinearAlgebra.RootSystem.Irreducible
 
 
 
--- Proof that the root pairing An is reduced
-lemma An_is_reduced (n : ℕ) [NeZero n]: (An n).IsReduced := by
-  sorry
-
-lemma  An_is_crystallographic (n : ℕ) [NeZero n]: (An n).IsCrystallographic := by
-  infer_instance
-
-instance An_is_finite (n : ℕ) [NeZero n] : Fintype (SignedInterval n) := by
+instance finite (n : ℕ) [NeZero n] : Fintype (SignedInterval n) := by
   exact Fintype.ofEquiv {x : (Fin n × Fin n) × Bool // x.1.1 ≤ x.1.2}
     { toFun := fun x =>
         { i := x.1.1.1
@@ -29,8 +22,68 @@ instance An_is_finite (n : ℕ) [NeZero n] : Fintype (SignedInterval n) := by
         cases J
         rfl }
 
-lemma finite (n : ℕ) [NeZero n] : Finite (SignedInterval n) := by
+lemma An_is_finite (n : ℕ) [NeZero n] : Finite (SignedInterval n) := by
   infer_instance
 
-lemma An__is_irreducible (n : ℕ) [NeZero n] : (An n).IsIrreducible := by
-  sorry
+-- Proof that the root pairing An is reduced
+lemma An_is_reduced (n : ℕ) [NeZero n] : (An n).IsReduced := by
+  rw [RootPairing.isReduced_iff]
+  intro J K hlin
+  have hcw : (An n).coxeterWeight J K = 4 := by
+    exact (RootPairing.coxeterWeight_eq_four_iff_not_linearIndependent (An n)).2 hlin
+  change ((Zn_pairing n (α J)) (α_dual K) * (Zn_pairing n (α K)) (α_dual J) = 4) at hcw
+  rw [pairing_formula K J, pairing_formula J K] at hcw
+  obtain ⟨Ji, Jj, Jh, Jε⟩ := J
+  obtain ⟨Ki, Kj, Kh, Kε⟩ := K
+  cases Jε <;> cases Kε
+  · simp +decide only [SignedInterval.sign] at hcw
+    have hends : Ji = Ki ∧ Jj = Kj := by
+      split_ifs at hcw <;> constructor <;> apply Fin.ext <;> omega
+    rcases hends with ⟨hi, hj⟩
+    subst Ki
+    subst Kj
+    left
+    rfl
+  · simp +decide only [SignedInterval.sign] at hcw
+    have hends : Ji = Ki ∧ Jj = Kj := by
+      split_ifs at hcw <;> constructor <;> apply Fin.ext <;> omega
+    rcases hends with ⟨hi, hj⟩
+    subst Ki
+    subst Kj
+    right
+    ext t
+    change α ({ i := Ji, j := Jj, hij := Jh, ε := false } : SignedInterval n) t =
+      - α ({ i := Ji, j := Jj, hij := Kh, ε := true } : SignedInterval n) t
+    rw [α_eval, α_eval]
+    simp [SignedInterval.sign]
+  · simp +decide only [SignedInterval.sign] at hcw
+    have hends : Ji = Ki ∧ Jj = Kj := by
+      split_ifs at hcw <;> constructor <;> apply Fin.ext <;> omega
+    rcases hends with ⟨hi, hj⟩
+    subst Ki
+    subst Kj
+    right
+    ext t
+    change α ({ i := Ji, j := Jj, hij := Jh, ε := true } : SignedInterval n) t =
+      - α ({ i := Ji, j := Jj, hij := Kh, ε := false } : SignedInterval n) t
+    rw [α_eval, α_eval]
+    simp [SignedInterval.sign]
+  · simp +decide only [SignedInterval.sign] at hcw
+    have hends : Ji = Ki ∧ Jj = Kj := by
+      split_ifs at hcw <;> constructor <;> apply Fin.ext <;> omega
+    rcases hends with ⟨hi, hj⟩
+    subst Ki
+    subst Kj
+    left
+    rfl
+
+lemma An_is_crystallographic (n : ℕ) [NeZero n] : (An n).IsCrystallographic := by
+  infer_instance
+
+
+
+-- Note that the root pairing An is not irreducible. Indeed, the set of roots for A1 is {2e1, -2e1},
+-- whose ℤ-span is a proper sub-module of M which remains invariant under the s_i's.
+-- Similarly, the root pairing An is not a root system.
+
+-- To fix this, we must base change to ℚ or ℝ
